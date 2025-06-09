d56d8942d8662a4dc6c5900a254857022b472dd0
Taylor Kothenbeutel <166166061+TKothenbeutel@users.noreply.github.com>
2025-06-09 16:14:41 +0000

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="unique_songs_spotify_helpers",
+    
+    version="0.1",
+
+    url="https://github.com/TKothenbeutel/Unique-Spotify-Songs",
+
+    author="Taylor Kothenbeutel",
+    
+    package_dir={"": "helpers"},
+    
+    packages=find_packages(where="helpers"),
+    
+    python_requires=">=3.7, <4",
+    
+    install_requires=["python-dateutil","spotipy"],
+)
